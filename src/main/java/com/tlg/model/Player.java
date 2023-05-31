637e03e679739e7c7d38464028e9f69c4cd8e5d8
Christopher Baker <christopher.baker@tlgcohort.com>
2023-05-31 14:28:00 -0700

    PR Fixed game crashing when you die in the first screen, Fixes AB#720

--- src/main/java/com/tlg/model/Player.java
+++ src/main/java/com/tlg/model/Player.java
@@ -86,6 +86,8 @@ public class Player {
     }
 
     public void useAmulet() {
-        location = prevLocation;
+        if (prevLocation != null) {
+            location = prevLocation;
+        }
     }
 }
