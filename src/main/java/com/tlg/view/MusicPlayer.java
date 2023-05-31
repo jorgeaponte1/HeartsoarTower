8a5e6732d7eb316795b4f99ac5c84a5d572cb0b2
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-05-31 13:55:48 -0700

    Game music can now be turned off and back on and key is now able to be added into inventory after calling get key, fixes AB#714, AB#716

--- src/main/java/com/tlg/view/MusicPlayer.java
+++ src/main/java/com/tlg/view/MusicPlayer.java
@@ -1,39 +1,36 @@
 package com.tlg.view;
 import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MusicPlayer {
     private Clip clip;
     private FloatControl volumeControl;
-    private String lastFilePath;
 
-    public void play(String filePath) {
-        stop();
-            try {
-//                For audio: Input stream is funky.  Needs buffered in order to go backwards.  Note:  Pictures will work very similar
-                InputStream input = getClass().getClassLoader().getResourceAsStream(filePath);
-//                Buffered allows us to use mark and reset
-                InputStream buffer = new BufferedInputStream(input);
-                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(buffer);
-                clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-                volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                clip.loop(Clip.LOOP_CONTINUOUSLY);
-                setVolume(0.75f);
-            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-                e.printStackTrace();
-            }
+    public MusicPlayer(String filePath) {
+        try {
+            InputStream input = getClass().getClassLoader().getResourceAsStream(filePath);
+            InputStream buffer = new BufferedInputStream(input);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(buffer);
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            setVolume(0.75f);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
         }
+    }
 
+    public void play() {
+        if (clip != null && !clip.isRunning()) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
 
     public void stop() {
         if (clip != null && clip.isRunning()) {
             clip.stop();
-            clip.close();
-            clip = null;
         }
     }
 
