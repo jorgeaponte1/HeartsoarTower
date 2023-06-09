9086cf9a2c4a4a0c27dee9059fb75f0383022be8
Jessica G <91770301+Gi-bi@users.noreply.github.com>
2023-06-09 16:13:52 -0700

    Layout and scaling updates

--- src/main/java/com/tlg/model/Room.java
+++ src/main/java/com/tlg/model/Room.java
@@ -136,9 +136,9 @@ public class Room {
         // Scale the image to the original scaled size
         //noinspection ConstantConditions
         ImageIcon originalIcon = new ImageIcon(imageUrl);
-        int newWidth = originalIcon.getIconWidth() / 3; // Adjust the divisor to match the scaling factor used for
+        int newWidth = originalIcon.getIconWidth() / 2; // Adjust the divisor to match the scaling factor used for
         // the original image
-        int newHeight = originalIcon.getIconHeight() / 3; // Adjust the divisor to match the scaling factor used for
+        int newHeight = originalIcon.getIconHeight() / 2; // Adjust the divisor to match the scaling factor used for
         // the original image
         Image scaledImage = originalIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
 
